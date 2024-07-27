f37f6cac19b36a87b176b78d46db26cc288d7c60
Eduardo Quelotti <eduardoquelotti@gmail.com>
2024-07-27 08:49:28 -0300

    Exclusão aula03.py

---
+++ bootcamp_python/aula03_exercicios.py
@@ -0,0 +1,19 @@
+# x = int(input("Please enter an integer: "))
+
+# if x < 0:
+#     x = 0
+#     print('Negative changed to zero')
+# elif x == 0:
+#     print('Zero')
+# elif x == 1:
+#     print('Single')
+# else:
+#     print('More')
+
+quantidade = 10
+preco = 20
+
+if quantidade > 0 and preco > 0:
+    print('Dados válidos inseridos.')
+else:
+    print('Dados inválidos.')
