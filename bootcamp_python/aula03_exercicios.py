f197771b05b9c86507bb8708032f95cd9c900d9b
Eduardo Quelotti <eduardoquelotti@gmail.com>
2024-07-28 13:31:33 -0300

    Exercícios aula 03 atualizados

--- bootcamp_python/aula03_exercicios.py
+++ bootcamp_python/aula03_exercicios.py
@@ -68,6 +68,15 @@
 # a R$ 10.000 ou se ocorrer fora do horário comercial (antes das 9h ou depois das 18h). 
 # Dada uma transação como `transacao = {'valor': 12000, 'hora': 20}`, verifique se ela é suspeita.
 
+# transacao = {'valor': 9000, 'hora': 10}
+
+
+# if transacao['valor'] > 10000 or transacao['hora'] < 9 or transacao['hora'] > 18:
+#     print('Transação suspeita')
+# else:
+#     print('Transação normal')
+
+
 ### Exercício 6. Contagem de Palavras em Textos
 # Objetivo:** Dado um texto, contar quantas vezes cada palavra única aparece nele.
 
