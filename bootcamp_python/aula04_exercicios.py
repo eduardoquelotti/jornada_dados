7fd953f1dc8a126834e8a4f76fa8e2a2ba6866d1
Eduardo Quelotti <eduardoquelotti@gmail.com>
2024-07-29 01:33:53 -0300

    Aula04 atualizada

--- bootcamp_python/aula04_exercicios.py
+++ bootcamp_python/aula04_exercicios.py
@@ -82,3 +82,11 @@
 #Opção 2:
 # total = sum(tabela_preco[item] for item in lista_compras)
 # print('O valor total da compra é: R$ ', total)
+
+
+
+# Dada uma lista de emails, remover todos os duplicados:
+# emails = ["user@example.com", "admin@example.com", "user@example.com", "manager@example.com"]
+# emails_unicos = list(set(emails))
+
+# print(emails_unicos)
