6fa96a5efad303f679bd1b608545057fa5fea02b
Eduardo Quelotti <eduardoquelotti@gmail.com>
2024-07-31 23:35:44 -0300

    atualiza

--- bootcamp_python/aula09_loguru.py
+++ bootcamp_python/aula09_loguru.py
@@ -1,5 +1,10 @@
 from loguru import logger
 
+logger.add(
+    "arquivo_logs.log",
+    format ="{time} | {level} | {message} | arquivo: {file}"
+    )
+
 logger.debug("Log de debug - Geralmente utilizado para informar ou relembrar algo")
 logger.info("Log de info - Geralmente utilizado para informações relevantes")
 logger.warning("Log de aviso - Geralmente utilizado para informar que algo pode dar errado (ex de uso: mensagens que uma biblioteca ou função será descontinuada)")
